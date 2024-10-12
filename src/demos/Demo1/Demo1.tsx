bddca9d028b23564d08031f93ba6f488586aef0a
jxk <v_jxkjiang@tencent.com>
2024-10-12 18:14:30 +0800

    props vs context

--- src/demos/Demo1/Demo1.tsx
+++ src/demos/Demo1/Demo1.tsx
@@ -38,11 +38,15 @@ function Demo1() {
             <Demo1Child title="消费背景色和边框色" />
             <Demo1Child1 title="只消费边框色，context使用useMemo传值" />
             <Demo1Child2 title="不消费任何颜色，不使用memo" />
+            <Demo1Child4 title="使用props传递" bg={bg}/>
           </Demo1Context.Provider>
         </Demo1Context2.Provider>
       </div>
       <div>
-        <p className="text-2xl">结论</p>
+        <p className="text-2xl">结论
+        1.使用context可以避免中间层级的多余渲染，避免不相干的状态变化触发中间层级渲染
+        2.传入context中的值需避免每次渲染时使用新的引用
+        3.使用memo,不仅能避免props不变时的多余渲染，context同理</p>
       </div>
     </div>
   );
@@ -117,4 +121,44 @@ const Demo1Child3 = memo(function (props: { title?: string }) {
     </div>
   );
 });
+
+
+let count4 = 0;
+
+const Demo1Child4 = memo(function (props: { title?: string ,bg:string}) {
+  const { title ,bg} = props;
+  const {  border } = useContext(Demo1Context2);
+  console.log(title, count4);
+
+  count4++;
+  return (
+    <div
+      className="p-10 m-2 text-white text-3xl  border-solid border-8"
+      style={{ background: 'red', borderColor: border }}
+    >
+      {title}:渲染次数：{count4}
+      <Demo1Child5 title="跨层级消费：props" bg={bg}/>
+    </div>
+  );
+});
+
+let count5 = 0;
+
+const Demo1Child5 = memo(function (props: { title?: string,bg:string }) {
+  const { title ,bg} = props;
+  const {  border } = useContext(Demo1Context);
+  console.log(bg, title, count5);
+
+  count5++;
+  return (
+    <div
+      className="p-10 m-2 text-white text-3xl border-solid border-8"
+      style={{ background: bg, borderColor: border }}
+    >
+      {title}:渲染次数：{count5}
+    </div>
+  );
+});
+
 export default Demo1;
+
